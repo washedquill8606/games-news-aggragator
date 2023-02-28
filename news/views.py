7d41f6f24eaa662a4c28f87d37cdaa0a265cd603
Alex Puga <scdo117ds@outlook.com>
2023-02-27 18:34:39 -0600

    only read, not link

--- news/views.py
+++ news/views.py
@@ -9,35 +9,57 @@ lu_soup = BeautifulSoup(lu_r.content, 'html5lib')
 
 lu_news = []
 lu_as = []
-lu_headings = lu_soup.find_all('h4')
+lu_list = []
+lu_dict = {}
 
-for tags in lu_headings:
-    aTags = tags.find_all('a', {'class':'rC'})
-    for aTag in aTags:
-        href = aTag.get('href')
-        if href is not None:
-            href_str = 'https://levelup.com' + href
-            # href_cpd = ""f"{href_str}""" (this is for passing the string with double quotes)
-            lu_as.append(aTag.get_text(strip=True)) #href_str
+#for texts
+lu_headings = lu_soup.find_all('h4', {'class' : 'elementHeader'})
 
 for lu_heading in lu_headings[:10]:
     lu_news.append(lu_heading.get_text(strip=True))
 
+#for links
+
+h4_links = [h4.a for h4 in lu_soup.find_all('h4', {'class' : 'elementHeader'})]
+for h4_link in h4_links:
+    href = h4_link.get('href')
+    href_str = 'https://levelup.com' + href
+    lu_as.append(href_str)
+        # href_cpd = ""f"{href_str}""" (this is for passing the string with double quotes)
+
+for x, y in zip(lu_news,lu_as):
+    lu_list += [x,y]
+    lu_dict = {
+        'title' : x,
+        'link' : y,
+    }
 
 #Eurogamer news
 eu_r = requests.get("https://www.eurogamer.es/news")
 eu_soup = BeautifulSoup(eu_r.content, 'html5lib')
 
 eu_news = []
-eu_headings = eu_soup.find_all('div', {'class':'summary'})
+eu_as = []
+
+#for texts
+
+eu_headings = eu_soup.find_all('p', {'class':'title'})
 
 for eu_heading in eu_headings[:10]:
     eu_news.append(eu_heading.get_text(strip=True))
+
+#for links
+
+h4eu_links = [h4.a for h4 in eu_soup.find_all('p', {'class' : 'title'})]
+for h4eu_link in h4eu_links:
+    eu_as.append(h4eu_link['href'])
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {
         'lu_news' : lu_news,
         'lu_as' : lu_as,
+        'lu_dict' : lu_dict,
         'eu_news' : eu_news,
+        'eu_as' : eu_as,
     })
